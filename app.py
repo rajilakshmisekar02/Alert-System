b16e36bb595f0446c6dc3e74f9b1c62b812dfd2c
rajilakshmisekar02 <rajilakshmisekar02@gmail.com>
2026-02-08 00:31:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,82 @@
+from flask import Flask, request, jsonify
 from twilio.rest import Client
 import os
 import time
-from flask import Flask
-from flask_socketio import SocketIO
 
 app = Flask(__name__)
-# cors_allowed_origins="*" is important for external devices like ESP32
-# socketio = SocketIO(app, cors_allowed_origins="*")
-# In your app.py
-socketio = SocketIO(app, cors_allowed_origins="*", ping_timeout=60, ping_interval=25)
+
+# Twilio Config
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_TOKEN = os.getenv("TWILIO_TOKEN")
+FROM_NUMBER = os.getenv("TWILIO_FROM")
+TO_NUMBER = os.getenv("CLIENT_NUMBER")
+
+client = Client(TWILIO_SID, TWILIO_TOKEN)
+
+GAS_THRESHOLD = 350
+WEIGHT_THRESHOLD = 2.0
+
+last_alert_time = 0
+ALERT_COOLDOWN = 300   # 5 minutes
+
+
+def send_sms(msg):
+    # try:
+    #     client.messages.create(
+    #         body=msg,
+    #         from_=FROM_NUMBER,
+    #         to=TO_NUMBER
+    #     )
+    #     print("SMS Sent")
+    # except Exception as e:
+    #     print("SMS Error:", e)
+    print(f'alert {msg}')
+
 
 @app.route('/')
-def index():
-    return "WebSocket Server is Running!"
+def home():
+    return "Alert System Running........"
 
-# This function runs when the ESP32 sends a message
-@socketio.on('pot_data')
-def handle_pot_value(data):
-    print(f"Potentiometer Value received: {data}")
 
-if __name__ == '__main__':
-    # Use '0.0.0.0' so it's accessible over the internet
-    socketio.run(app, host='0.0.0.0', port=5000)
+@app.route('/data', methods=['POST'])
+def receive_data():
+    global last_alert_time
+
+    data = request.get_json()
+    print("Received:", data)
+
+    gas = data.get("gas", 0)
+    fire = data.get("fire", False)
+    weight = data.get("weight", 0)
+    device = data.get("device_id", "ESP8266")
+
+    alert = False
+
+    msg = f"Alert from {device}\n\n"
+    msg += f"Gas Level: {gas}\n"
+    msg += f"Fire Status: {'YES' if fire else 'NO'}\n"
+    msg += f"Weight Level: {weight} kg\n\n"
+
+    if gas > GAS_THRESHOLD:
+        msg += "⚠ Gas Leak Detected\n"
+        alert = True
+
+    if fire:
+        msg += "🔥 Fire Detected\n"
+        alert = True
+
+    if weight < WEIGHT_THRESHOLD:
+        msg += "⚠ Low Cylinder Level\n"
+        alert = True
+
+    current = time.time()
+
+    if alert and (current - last_alert_time) > ALERT_COOLDOWN:
+        send_sms(msg)
+        last_alert_time = current
+
+    return jsonify({"status": "ok"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
